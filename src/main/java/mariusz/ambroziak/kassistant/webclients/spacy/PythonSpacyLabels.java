f3357eb292ec90919f01f28bf4304f7430f768fa
spike <mariuszambroziak667@gmail.com>
2020-05-18 09:33:09 +0200

    Changes made to adhere to (seemingly) changes in tags in Spacy

--- src/main/java/mariusz/ambroziak/kassistant/webclients/spacy/PythonSpacyLabels.java
+++ src/main/java/mariusz/ambroziak/kassistant/webclients/spacy/PythonSpacyLabels.java
@@ -2,9 +2,10 @@ package mariusz.ambroziak.kassistant.webclients.spacy;
 
 public class PythonSpacyLabels {
 	public static String tokenisationCardinalLabel="CD";
-	
-	
-	
+	public static String listItemMarker="LS";
+
+
+
 	public static String entitiesCardinalLabel="CARDINAL";
 
 	
