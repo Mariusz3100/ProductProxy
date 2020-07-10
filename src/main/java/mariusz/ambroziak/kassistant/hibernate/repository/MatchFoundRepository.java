17d8505929c7700e526f546d2ee176ea97ebc8b2
spike <mariuszambroziak667@gmail.com>
2020-07-10 02:15:37 +0200

    Saving MatchFound.

---
+++ src/main/java/mariusz/ambroziak/kassistant/hibernate/repository/MatchFoundRepository.java
@@ -0,0 +1,10 @@
+package mariusz.ambroziak.kassistant.hibernate.repository;
+
+import mariusz.ambroziak.kassistant.hibernate.model.MatchFound;
+import mariusz.ambroziak.kassistant.hibernate.model.ParsingBatch;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MatchFoundRepository extends CrudRepository<MatchFound,Long> {
+
+
+}
