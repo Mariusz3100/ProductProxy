8aac93698617688928d08fda0743f4c19a833847
spike <mariuszambroziak667@gmail.com>
2020-07-15 04:23:18 +0200

    New Constants for recognizing nouns in spacy tokens

--- src/main/java/mariusz/ambroziak/kassistant/constants/NlpConstants.java
+++ src/main/java/mariusz/ambroziak/kassistant/constants/NlpConstants.java
@@ -5,7 +5,9 @@ public class NlpConstants {
 	
 	public static final String connectiveTag="IN";
 	public static final String connectivePos="ADP";
-	
-	
+	public static final String nounPos="NOUN";
+
+
+	public static final String pluralNounTag="NNS";
 	public static final String of_Word="of";
 }
