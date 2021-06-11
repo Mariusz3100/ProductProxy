c0db26c39f90ecb074484e3d711ee32174d7f298
spike <mariuszambroziak667@gmail.com>
2021-06-11 21:22:37 +0200

    various files???

--- src/main/java/mariusz/ambroziak/kassistant/webclients/rapidapi/RapidApiClient.java
+++ src/main/java/mariusz/ambroziak/kassistant/webclients/rapidapi/RapidApiClient.java
@@ -4,6 +4,7 @@ public abstract class RapidApiClient {
 
 	protected static final String header1Name = "X-RapidAPI-Host";
 	protected static final String header2Name = "X-RapidAPI-Key";
-	protected static final String header2Value = "13c1e833admsh8aaf0fcc8c9193cp1b7fecjsna0b6b86e6b71";
+	//protected static final String header2Value = "13c1e833admsh8aaf0fcc8c9193cp1b7fecjsna0b6b86e6b71";
+	protected static final String header2Value = "5a3f04b153msh2b0a5d25d0330adp1692bejsn44cfe9058a64";
 
 }
